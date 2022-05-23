101d279f920062156981ffe366d04ebc61c7a3eb
OmarFaruqueSabbir <avrosabbir18@gmail.com>
2022-05-23 11:10:56 +0600

    POST operation done for orders in OrdersCollection

--- index.js
+++ index.js
@@ -9,67 +9,91 @@ app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a2urmj8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-      return res.status(401).send({ message: 'UnAuthorized access' });
+        return res.status(401).send({ message: 'UnAuthorized access' });
     }
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-      if (err) {
-        return res.status(403).send({ message: 'Forbidden access' })
-      }
-      req.decoded = decoded;
-      next();
-    });
-  }
-
-    async function run() {
-        try {
-            await client.connect();
-            const toolsCollection = client.db('manufacturer').collection('tools');
-            console.log('Database Connected!')
-            const userCollection = client.db('manufacturer').collection('users');
-            console.log('Database Connected!')
-    
-            //Get Tools
-            app.get("/tools", async (req, res) => {
-                const tools = await toolsCollection.find({}).toArray();
-                res.send(tools);
-              });
-
-            //set user in Database
-            app.put('/user/:email', async (req, res) => {
-                const email = req.params.email
-                const user = req.body
-                const filter = { email: email }
-                const options = { upsert: true }
-                const updateDoc = {
-                    $set: user,
-                };
-                const result = await userCollection.updateOne(filter, updateDoc, options)
-                const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
-                res.send({ result, token });
-    
-            })
-
-    
-        } finally {
-    
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
         }
-    
+        req.decoded = decoded;
+        next();
+    });
+}
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db('manufacturer').collection('tools');
+        console.log('Database Connected!')
+        const userCollection = client.db('manufacturer').collection('users');
+        const ordersCollection = client.db('manufacturer').collection('orders');
+
+
+
+        //set user in Database
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
+            res.send({ result, token });
+        })
+
+        //Get Tools
+        app.get("/tools", async (req, res) => {
+            const tools = await toolsCollection.find({}).toArray();
+            res.send(tools);
+        });
+
+        //get single tools
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
+        });
+
+
+        // post orders
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const query = { tool: order.tool, user: order.user }
+            const exists = await ordersCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, data: exists })
+            }
+            const result = await ordersCollection.insertOne(order);
+            return res.send({ success: true, result });
+        })
+
+
+
+
+    } finally {
+
     }
-    run().catch(console.dir);
+
+}
+run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Running Manufacturer Server');
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Listening to port', port);
 })
